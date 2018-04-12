92f5d54795e2b2803e7e50bc6d01ad60eba49c7c
burgioandrea <andreaburgio1993@gmail.com>
2018-04-12 14:12:46 +0200

    Creazione classi e proprietà

---
+++ LabEF/LabEF.ConsoleApp/Models/Fattura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabEF.ConsoleApp.Models
+{
+    [Table("Fatture")]
+    
+
+    public class Fattura
+    {
+        public int FatturaId { get; set; }
+
+        [Column("Data")]
+        public DateTime DataFattura { get; set; }
+
+        [Column("Num")]
+        public string NumeroFattura { get; set; }
+
+
+        //altri dati
+
+        public List<VoceFattura> VociFattura { get; set; }
+
+
+
+    }
+}
---
+++ LabEF/LabEF.ConsoleApp/Models/VoceFattura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabEF.ConsoleApp.Models
+{
+    public class VoceFattura
+    {
+        public int VoceFatturaID { get; set; }
+
+        public int Qta { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public Double Imponibile { get; set; }
+
+
+
+    }
+}
